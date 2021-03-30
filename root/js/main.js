2668284123da234d951eb90f813c90739f7ad029
jasonaviandre <jasonaviandre@gmail.com>
2021-03-30 11:41:43 +0800

    update udemy npm

--- root/js/main.js
+++ root/js/main.js
@@ -1,3 +1,8 @@
+var _ = require('lodash');
+
+var array = [1,2,3,4,5,6,7,8];
+console.log('answer:', _.without(array, 3))
+
 window.addEventListener("scroll", function() {
     var header = document.querySelector(".container");
     var logo = document.querySelector(".logo img");
